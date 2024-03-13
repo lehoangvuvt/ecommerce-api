69faca9e54004e30f66a462002d8d6312b0efdf0
VuLH <lehoangvu@bacoor.co>
2024-03-13 09:22:31 +0700

    fix: add .env

--- src/services/rabbitmq.service.ts
+++ src/services/rabbitmq.service.ts
@@ -6,22 +6,19 @@ export default class RabbitMqService {
 
   connect(callback: (error: any, channel: Channel) => void) {
     const rabbitMq = amqplib
-    rabbitMq.connect(
-      `amqp://eMUhSo5hDLWaje4S:oyek43jLD7IWrmkvFycxARTsY-iZGJ2d@rabbitmq.railway.internal:5672`,
-      (connectError: any, connection: amqplib.Connection) => {
-        if (connectError) {
-          throw connectError
-        }
-        connection.createChannel((createChannelError: any, channel: Channel) => {
-          if (createChannelError) {
-            callback(createChannelError, null)
-            throw createChannelError
-          }
-          this.rabbitMqChannel = channel
-          callback(null, this.rabbitMqChannel)
-        })
+    rabbitMq.connect(process.env.RABBIT_MQ_CONNECTION_STRING, (connectError: any, connection: amqplib.Connection) => {
+      if (connectError) {
+        throw connectError
       }
-    )
+      connection.createChannel((createChannelError: any, channel: Channel) => {
+        if (createChannelError) {
+          callback(createChannelError, null)
+          throw createChannelError
+        }
+        this.rabbitMqChannel = channel
+        callback(null, this.rabbitMqChannel)
+      })
+    })
   }
 
   getChannel() {
