9025a53a7922a3b36f4226c70ba5c965d9d7bafb
VuLH <lehoangvu@bacoor.co>
2024-04-04 14:56:37 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,9 @@ import express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.use(cors({ credentials: true, origin: [process.env.CLIENT_HOST_URL, process.env.CLIENT_ADMIN_URL] }))
-  app.use(cookieParser())
+  app.use(cors({ credentials: true, origin: process.env.CLIENT_HOST_URL }))
+  app.use(cookieParser)
   app.useGlobalPipes(new ValidationPipe())
-  app.use(express.json({ limit: '50mb' }))
-  app.use(express.urlencoded({ limit: '50mb' }))
   const config = new DocumentBuilder().setTitle('Ecommerce API').setDescription('API for ecommerce').setVersion('1.0').build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api-documents', app, document)
--- src/services/rabbitmq.service.ts
+++ src/services/rabbitmq.service.ts
@@ -5,20 +5,20 @@ export default class RabbitMqService {
   constructor() {}
 
   connect(callback: (error: any, channel: Channel) => void) {
-    const rabbitMq = amqplib
-    rabbitMq.connect(process.env.RABBIT_MQ_CONNECTION_STRING, (connectError: any, connection: amqplib.Connection) => {
-      if (connectError) {
-        throw connectError
-      }
-      connection.createChannel((createChannelError: any, channel: Channel) => {
-        if (createChannelError) {
-          callback(createChannelError, null)
-          throw createChannelError
-        }
-        this.rabbitMqChannel = channel
-        callback(null, this.rabbitMqChannel)
-      })
-    })
+    // const rabbitMq = amqplib
+    // rabbitMq.connect(process.env.RABBIT_MQ_CONNECTION_STRING, (connectError: any, connection: amqplib.Connection) => {
+    //   if (connectError) {
+    //     throw connectError
+    //   }
+    //   connection.createChannel((createChannelError: any, channel: Channel) => {
+    //     if (createChannelError) {
+    //       callback(createChannelError, null)
+    //       throw createChannelError
+    //     }
+    //     this.rabbitMqChannel = channel
+    //     callback(null, this.rabbitMqChannel)
+    //   })
+    // })
   }
 
   getChannel() {
